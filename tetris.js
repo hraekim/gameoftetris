a5df7f9b085451f541688bb30dbcba24a94fc83c
Kevin Kim <kevinkim@Kevins-MacBook-Air.local>
2019-01-17 23:27:05 -0500

    colorrrrrrs

--- tetris.js
+++ tetris.js
@@ -224,3 +224,14 @@ document.addEventListener('keydown', event => {
         playerRotate(1);
     }
 });
+
+const colors = [
+    null,
+    '#FF0D72',
+    '#0DC2FF',
+    '#0DFF72',
+    '#F538FF',
+    '#FF8E0D',
+    '#FFE138',
+    '#3877FF',
+];
