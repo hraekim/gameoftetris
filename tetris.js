123d353c6af2316dd633a58bfa4568e1d07df40e
Kevin Kim <kevinkim@Kevins-MacBook-Air.local>
2019-01-17 23:09:35 -0500

    rotating metrix

--- tetris.js
+++ tetris.js
@@ -121,3 +121,23 @@ function merge(arena, player) {
         });
     });
 }
+
+function rotate(matrix, dir) {
+    for (let y = 0; y < matrix.length; ++y) {
+        for (let x = 0; x < y; ++x) {
+            [
+                matrix[x][y],
+                matrix[y][x],
+            ] = [
+                matrix[y][x],
+                matrix[x][y],
+            ];
+        }
+    }
+
+    if (dir > 0) {
+        matrix.forEach(row => row.reverse());
+    } else {
+        matrix.reverse();
+    }
+}
