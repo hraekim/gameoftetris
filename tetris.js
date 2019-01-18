9f38c6d33c2d5a2d761ff9035601e84c3b5b0251
Kevin Kim <kevinkim@Kevins-MacBook-Air.local>
2019-01-17 23:27:32 -0500

    score + ending

--- tetris.js
+++ tetris.js
@@ -235,3 +235,15 @@ const colors = [
     '#FFE138',
     '#3877FF',
 ];
+
+const arena = createMatrix(12, 20);
+
+const player = {
+    pos: {x: 0, y: 0},
+    matrix: null,
+    score: 0,
+};
+
+playerReset();
+updateScore();
+update();
