464ab08f25fe9af63746357f7623fe823c51baa5
Kevin Kim <kevinkim@Kevins-MacBook-Air.local>
2019-01-17 23:26:39 -0500

    keydown element

--- tetris.js
+++ tetris.js
@@ -210,3 +210,17 @@ function update(time = 0) {
 function updateScore() {
     document.getElementById('score').innerText = player.score;
 }
+
+document.addEventListener('keydown', event => {
+    if (event.keyCode === 37) {
+        playerMove(-1);
+    } else if (event.keyCode === 39) {
+        playerMove(1);
+    } else if (event.keyCode === 40) {
+        playerDrop();
+    } else if (event.keyCode === 81) {
+        playerRotate(-1);
+    } else if (event.keyCode === 87) {
+        playerRotate(1);
+    }
+});
